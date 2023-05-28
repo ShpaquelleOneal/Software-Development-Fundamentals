f72a87551c072e2a6984ba0dedb266924c248759
ShpaquelleOneal <109593992+ShpaquelleOneal@users.noreply.github.com>
2023-05-28 13:45:57 +0300

    2/4 test cases passed

--- APTS_Tasks/Task_2_team/main _queue.cpp
+++ APTS_Tasks/Task_2_team/main _queue.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
+
 #include <queue>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ struct Node {
     int data;
     Node* left;
     Node* right;
-    ~Node() { // desctructor
+    ~Node() { // Desctructor
         delete left;
         delete right;
     }
@@ -24,9 +25,8 @@ Node* createNode(int value) {
 }
 
 
-// function to find required node by given value recursively
+// Function to find required node by given value recursively
 Node* findNode(int value, Node* root) {
-    //cout << "test"<< endl;
     if (root == nullptr) return nullptr;
 
     if (root->data == value) return root;
@@ -38,13 +38,13 @@ Node* findNode(int value, Node* root) {
     return rightResult;
 }
 
-// Function to display data on each level of a binary tree
+// Function to display data on each level of a binary tree in reverse order
 void displayLevelsReverse(Node* root, ofstream& fout) {
     if (root == nullptr)
         return;
 
     queue<Node*> q;
-    stack<vector<int>> levels; // Stack to store levels in reverse order
+    vector<vector<int>> levels; // Vector of vectors to store levels
 
     q.push(root);
     int levelCount = -1; // Counter for level number
@@ -57,7 +57,7 @@ void displayLevelsReverse(Node* root, ofstream& fout) {
             Node* current = q.front();
             q.pop();
 
-            levelData.push_back(current->data); // Store current node's data
+            levelData.push_back(current->data);
 
             if (current->left)
                 q.push(current->left);
@@ -67,21 +67,32 @@ void displayLevelsReverse(Node* root, ofstream& fout) {
             size--;
         }
 
-        if (!levelData.empty()) // Add this condition to avoid pushing empty levels
-            levels.push(levelData); // Push current level data to the stack
+        if (!levelData.empty()) { // Add this condition to avoid pushing empty levels
+            // Sort the data in array
+            for (int i = 0; i < levelData.size() - 1; i++) {
+                for (int j = i + 1; j < levelData.size(); j++) {
+                    if (levelData[i] > levelData[j]) {
+                        int temp = levelData[i];
+                        levelData[i] = levelData[j];
+                        levelData[j] = temp;
+                    }
+                }
+            }
+
+            levels.push_back(levelData); // Push current level data to the vector
+        }
 
         levelCount++; // Increment the level number
     }
 
-    while (!levels.empty()) {
-        vector<int> levelData = levels.top();
-        levels.pop();
+    for (int i = levels.size() - 1; i >= 0; i--) {
+        vector<int> levelData = levels[i];
 
         fout << levelCount << ": ";
 
-        for (int i = 0; i < levelData.size(); i++) {
-            fout << levelData[i];
-            if (i < levelData.size() - 1) {
+        for (int j = 0; j < levelData.size(); j++) {
+            fout << levelData[j];
+            if (j < levelData.size() - 1) {
                 fout << " ";
             }
         }
@@ -93,11 +104,11 @@ void displayLevelsReverse(Node* root, ofstream& fout) {
 
 int main() {
 
-    ifstream fin("team.in");
-    ofstream fon("team.out");
+    ifstream fin("team.in.txt");
+    ofstream fon("team.out.txt");
 
     /*
-    // check if input files have been opened
+    // Check if input files have been opened
     if (!fin.is_open() || !fon.is_open()) {
         fon << "Error opening files..." << endl;
         return 0;
@@ -108,38 +119,36 @@ int main() {
     Node * first;
     int levels = 0;
 
-    while(fin >> p >> w >> m) {// while there is data, read it
+    while(fin >> p >> w >> m) {// While there is data, read it
 
-        if (p == 0  && w == 0 && m == 0) {// if data consists of 3 zeros, it's the end of the data (given in the task)
+        if (p == 0  && w == 0 && m == 0) {// If data consists of 3 zeros, it's the end of the data (given in the task)
             break;
         }
 
-        //cout << p<<w<<m << endl;
-        if (!first_d) { // for every data entry other than first one
+        if (!first_d) { // For every data entry other than first one
 
-            if (w != 0) { // add data for women - left
-                //cout << "Add a "<< w <<" to the left" << endl;
+            if (w != 0) { // Add data for women - left
                 Node* select = findNode(p, first);
                 select->left = createNode(w);
             }
-            if (m != 0 ) { // add data for men - right
-                //cout << "Add a "<< m <<" to the right" << endl;
+            if (m != 0 ) { // Add data for men - right
                 Node* select = findNode(p, first);
                 select->right = createNode(m);
             }
 
-        } else { // if it's the first data entry, create as first element
+        } else { // If it's the first data entry, create as first element
             Node* root = createNode(p);
             root->left = createNode(w);
             root->right = createNode(m);
 
-            first = root; // store the first Node
+            first = root; // Store the first Node
             first_d = false;
         }
     }
 
-
+    // Use the function
     displayLevelsReverse(first, fon);
+
     fin.close();
     fon.close();
     delete first;
